be4cc75fdc9df157aca05e8518299618350d6ed9
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-10-18 12:02:10 +0600

    Added React, Firebase, React Router and React Bootstrap and a component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () =>
+{
+    return (
+        <div>
+            <h2>This is Home Page</h2>
+        </div>
+    );
+};
+
+export default Home; <h2>This is Home Page</h2>
